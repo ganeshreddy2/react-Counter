6ed1d8470e777581192118b156d7ab0bf15fa642
ganeshreddy2 <git@stackblitz.com>
2024-12-25 13:25:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-Counter
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ganeshreddy2/react-Counter)
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./style.css";
+import Counter from './components/Counter'
+
+const App = () => {
+  return <Counter />
+}
+
+export default App
